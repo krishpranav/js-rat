2c9785ea54f651578098f248e2e498a9c6238971
krishpranav <krisna.pranav@gmail.com>
2021-04-22 17:11:49 +0530

    update proto type string value

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,3 +137,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 });
 
+String.prototype.replaceAll = function(str1, str2, ignore){
+	return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+}
