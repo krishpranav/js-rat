c5768f03d9f2fc17fef1e7bfb96d3422c4b76681
krishpranav <krisna.pranav@gmail.com>
2021-04-22 07:36:02 -0400

    updated add listener function

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+	let body = document.getElementsByTagName("body")[0];
+	let divPasscodeWrapper = document.getElementsByClassName("passcode-wrapper")[0];
+	let spanPasscode = document.getElementsByClassName("passcode-text")[0];
+	let buttonPasscode = document.getElementsByClassName("passcode-button");
+	let buttonAction = document.getElementsByClassName("action-button");
+	let buttonClose = document.getElementsByClassName("close-button")[0];
+	let buttonSettings = document.getElementsByClassName("settings-button");
+	let divSettingsWrapper = document.getElementsByClassName("settings-wrapper")[0]
+    let divOutputWrapper = document.getElementsByClassName("output-wrapper")[0];
+
+}
