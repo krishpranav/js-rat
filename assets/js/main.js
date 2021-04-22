478ef310641df46aef6f0b0ea308c15ae3eb18bf
krishpranav <krisna.pranav@gmail.com>
2021-04-22 17:10:15 +0530

    update finding different os

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,33 @@ document.addEventListener("DOMContentLoaded", function() {
 	let divSettingsWrapper = document.getElementsByClassName("settings-wrapper")[0]
     let divOutputWrapper = document.getElementsByClassName("output-wrapper")[0];
 
+	if (detectMobile()) {
+		body.id = "mobile";
+	}
+	else {
+		body.id = "desktop";
+	}
+
+	for(let i = 0; i < buttonPasscode.length; i++) {
+		buttonPasscode[i].addEventListener("click", function() {
+			if(!this.classList.contains("clear") && !this.classList.contains("backspace")) {
+				let current = spanPasscode.getAttribute("data-passcode");
+				if(current === null) {
+					current = "";
+				}
+				spanPasscode.setAttribute("data-passcode", current + this.textContent);
+				spanPasscode.textContent += "•";
+			}
+			else if(this.classList.contains("clear")) {
+				spanPasscode.setAttribute("data-passcode", "");
+				spanPasscode.textContent = "";
+			}
+			else if(this.classList.contains("backspace")) {
+				let current = spanPasscode.getAttribute("data-passcode");
+				spanPasscode.setAttribute("data-passcode", current.substring(0, current.length - 1));
+				spanPasscode.textContent = spanPasscode.textContent.substring(0, spanPasscode.textContent.length - 1);
+			}
+		});
+	}
+
 }
